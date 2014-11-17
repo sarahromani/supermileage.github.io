beed7a2872aaf77810ecaa40f2b1abde629f384b
Michael Thiesen <micthiesen@gmail.com>
2014-11-16 16:03:01 -0800

    Building production environment :)

---
+++ gruntfile.js
@@ -0,0 +1,63 @@
+module.exports = function(grunt) {
+  grunt.initConfig({
+    clean: {
+      dist: ['build/*']
+    },
+    copy: {
+      bootstrap: {
+        cwd: 'bower_components/bootstrap/dist/',
+        expand: true,
+        src: ['fonts/*', 'js/bootstrap.min.js'],
+        dest: 'build/'
+      },
+      fontAwesome: {
+        cwd: 'bower_components/components-font-awesome/',
+        expand: true,
+        src: ['fonts/*'],
+        dest: 'build/'
+      },
+      jquery: {
+        cwd: 'bower_components/jquery/dist/',
+        expand: true,
+        src: ['jquery.min.js'],
+        dest: 'build/js/'
+      },
+    },
+    less: {
+      dev: {
+        files: {
+          'build/style.css': 'style.less'
+        }
+      },
+      prod: {
+        options: {
+          compress: true,
+          cleancss: true
+        },
+        files: {
+          'build/style.css': 'style.less'
+        }
+      }
+    },
+    watch: {
+      less: {
+        files: ['style.less'],
+        tasks: ['less:dev']
+      }
+    }
+  });
+
+  grunt.loadNpmTasks('grunt-contrib-clean');
+  grunt.loadNpmTasks('grunt-contrib-copy');
+  grunt.loadNpmTasks('grunt-contrib-less');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+
+  grunt.registerTask(
+    'default',
+    ['clean:dist', 'copy', 'less:dev', 'watch']
+  );
+  grunt.registerTask(
+    'build',
+    ['clean:dist', 'copy', 'less:prod']
+  );
+};
